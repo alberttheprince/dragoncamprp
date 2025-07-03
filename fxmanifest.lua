962a273649fbf808bb0cc1aad329313741e61e68
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2025-07-03 01:22:39 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'PrinceAlbert - Popcorn RP'
+description 'Resource to provide a more natural camera for larger dragon/wyvern peds'
+version '1.0.0'
+
+client_scripts {
+    'client.lua',
+}
